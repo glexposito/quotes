d61c73b76d7117714cead2995dba0735d176ad31
Guillermo <glexposito@gmail.com>
2023-03-11 21:53:31 +1300

    Add Quotes page.
    -Add Header component.
    -Add Footer component.
    -Add Layout component.
    -Add Bootstrap.
    -Add hardcoded quote.

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="mb-auto">
+            <div>
+                <h3 className="mb-0">Quotes</h3>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "./Footer"
+import Header from "./Header"
+
+function Layout({ children }: React.PropsWithChildren<{}>) {
+    return (
+        <>
+            <div className="d-flex h-100 text-center text-bg-dark">
+                <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+                    <Header />
+                    {children}
+                    <Footer />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Layout
