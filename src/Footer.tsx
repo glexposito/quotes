2792dfbe417f490d99c4b4aac0b2b2001db0db0d
Guillermo <glexposito@gmail.com>
2023-03-12 18:22:10 +1300

    Move export directives before declarations. (#4)
    - Move export directives before functional component declarations.

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,7 @@
-function Footer(): JSX.Element {
+export default function Footer() {
   return (
     <footer className="mt-auto text-white-50">
       <p>Just words from a text or speech written or spoken by another person.</p>
     </footer>
   );
 }
-
-export default Footer;
