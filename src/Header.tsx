ce0404b6de9744e1bf8e73812e446a283b7485bd
Guillermo <glexposito@gmail.com>
2023-03-17 20:55:40 +1300

    Add app version.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,11 @@
+import { version as version } from '../package.json';
+
 export default function Header() {
   return (
     <header className="mb-auto">
       <div>
         <h3 className="mb-0">Quotes</h3>
+        <p>v{version}</p>
       </div>
     </header>
   );
