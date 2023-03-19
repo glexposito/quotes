3b7bd355f659323f5714d753657366512dcca8f5
Guillermo <glexposito@gmail.com>
2023-03-19 21:00:56 +1300

    Load quotes from public api. (#21)
    - Load quotes from public api.
    - Show loading spinner when data is being loaded.
    - Show error message when api returns an error.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
 describe('App', () => {
-  it('should render App component correctly', () => {
+  it('should render App component correctly', async () => {
+    const promise = Promise.resolve();
+
     render(<App />);
-    const element = screen.getByRole('main');
-    expect(element).toBeInTheDocument();
+
+    await act(async () => {
+      await promise;
+    });
   });
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Layout from './Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Layout from './components/Layout';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
