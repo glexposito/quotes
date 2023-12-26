7729abfc7b251ec0684621631b7144fe5ee4e735
Guillermo <glexposito@gmail.com>
2023-12-27 10:53:22 +1300

    Fix "show error" (#76)

--- src/components/Quote.test.tsx
+++ src/components/Quote.test.tsx
@@ -1,48 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Quote from './Quote';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { useRandomQuoteQuery } from './quoteQuery';
 
 jest.mock('./quoteQuery');
 const mockedUseRandomQuoteQuery = useRandomQuoteQuery as jest.Mock;
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false
-    }
-  }
-});
-
-const wrapper = ({ children }: React.PropsWithChildren) => (
-  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-);
 
 describe('Quote component', () => {
   it('should display the loading spinner when data is being loaded', () => {
     mockedUseRandomQuoteQuery.mockImplementation(() => ({
       isLoading: true
     }));
-    render(<Quote />, { wrapper });
+    render(<Quote />);
     expect(screen.getByText('Loading...')).toBeInTheDocument();
   });
 
   it('should display the quote when api returns ok', () => {
     mockedUseRandomQuoteQuery.mockImplementation(() => ({
       data: { author: 'Aristotle', content: 'Knowing yourself is the beginning of all wisdom.' },
-      status: 'loading'
+      isError: false
     }));
-    render(<Quote />, { wrapper });
+    render(<Quote />);
     expect(screen.getByText('Aristotle')).toBeInTheDocument();
   });
 
   it('should display an error message when api returns an error', () => {
     mockedUseRandomQuoteQuery.mockImplementation(() => ({
-      data: { author: 'Aristotle', content: 'Knowing yourself is the beginning of all wisdom.' },
-      error: '404'
+      isError: true
     }));
-    render(<Quote />, { wrapper });
+    render(<Quote />);
     expect(
       screen.getByText('Well this is embarrassing, there was an error getting your random quote.')
     ).toBeInTheDocument();
